ac1d7c59ac1999cc1ba7b5c956895b2402a3e8af
BrunaBudel <bruna.budel@houden.com.br>
2024-12-10 11:01:18 -0300

    style: date font size

--- frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
+++ frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -42,7 +42,7 @@ export function DestinationAndDateStep({ closeGuestsInput, isGuestsInputOpen, op
             </div>
             <button onClick={openDatePicker} disabled={isGuestsInputOpen} className='flex items-center gap-2 text-left w-[220px]'>
                 <Calendar className='size-5 text-zinc-400' />
-                <span className=" text-lg text-zinc-400 w-40 flex-1">
+                <span className="  text-zinc-400 w-40 flex-1">
                     {displayedDate || 'Quando?'}
                 </span>
             </button>
